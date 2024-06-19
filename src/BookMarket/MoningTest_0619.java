00ab9e2e8418390d10e72d13efee61c3f3b2474f
shin-heewon <ise0305@naver.com>
2024-06-19 10:47:21 +0900

    모닝테스트 0619 메뉴 선택 결과 출력

--- src/BookMarket/MoningTest_0619.java
+++ src/BookMarket/MoningTest_0619.java
@@ -23,8 +23,8 @@ public class MoningTest_0619 {
 
         System.out.println("****************************************************");
 
-        System.out.println("              Welcome to Shopping Mall              ");
-        System.out.println("              Welcome to Book Market!          ");
+        System.out.println("\t\t"+greeting);
+        System.out.println("\t\t"+tagline);
 
         System.out.println("****************************************************");
         System.out.println("1. 고객 정보 확인하기       5. 바구니에 항목 추가하기");
@@ -34,9 +34,20 @@ public class MoningTest_0619 {
         System.out.println("****************************************************");
 
         System.out.print("메뉴 번호를 선택해주세요 ");
-        String menuNum = sc.next();
+        int menuNum = Integer.parseInt(sc.next());;
         System.out.println(menuNum+"번을 선택했습니다.");
 
 
+        switch (menuNum){
+            case 1 -> System.out.println("1. 고객 정보 확인하기");
+            case 2 -> System.out.println("2. 장바구니 상품 목록 보기");
+            case 3 -> System.out.println("3. 장바구니 비우기");
+            case 4 -> System.out.println("4. 영수증 표시하기");
+            case 5 -> System.out.println("5. 바구니에 항목 추가하기");
+            case 6 -> System.out.println("6. 장바구니의 항목 수량 줄이기");
+            case 7 -> System.out.println("7. 장바구니의 항목 삭제하기");
+            case 8 -> System.out.println("8. 종료");
+
+        }
     }
 }
